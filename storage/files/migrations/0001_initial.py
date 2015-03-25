4beee267216baf3aaabaae3f5ded2cdac0fbd55c
anna <null>
2015-03-25 21:08:56 +0300

    Remove admin

--- storage/files/migrations/0001_initial.py
+++ storage/files/migrations/0001_initial.py
@@ -2,19 +2,26 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
+from django.conf import settings
 
 
 class Migration(migrations.Migration):
 
     dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
     operations = [
         migrations.CreateModel(
-            name='File',
+            name='Upload',
             fields=[
-                ('id', models.AutoField(verbose_name='ID', primary_key=True, serialize=False, auto_created=True)),
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('file', models.FileField(upload_to='%Y/%m/%d')),
+                ('name', models.CharField(max_length=256)),
+                ('uuid', models.CharField(max_length=256)),
+                ('content_type', models.CharField(max_length=256, blank=True)),
+                ('md5sum', models.CharField(max_length=32)),
+                ('user', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
             ],
             options={
             },
